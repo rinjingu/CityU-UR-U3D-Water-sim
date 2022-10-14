7c462ec992ee827561be49dd170d098694244522
Rin Jingu <lamtingkwan2002@gmail.com>
2022-10-14 12:09:05 +0800

    Update
    Changes:
    -New Resource folder
    -Cam switch key changed to "F3"

--- Water sim/Assets/Script/cameraSwitch.cs
+++ Water sim/Assets/Script/cameraSwitch.cs
@@ -29,13 +29,13 @@ public class cameraSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //If the Tab button is pressed, switch to the next camera
+        //If the button is pressed, switch to the next camera
         //Set the camera at the current index to inactive, and set the next one in the array to active
         //When we reach the end of the camera array, move back to the beginning or the array.
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.F3))
         {
             currentCameraIndex++;
-            Debug.Log("Tab button has been pressed. Switching to the next camera");
+            Debug.Log("Button has been pressed. Switching to the next camera");
             if (currentCameraIndex < cameras.Length)
             {
                 cameras[currentCameraIndex - 1].gameObject.SetActive(false);
